a886014454a8bff37cdb3d79c65f3712af6dfda6
tarcea <tarcea@gmail.com>
2023-06-29 21:29:37 +0200

    login added

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,10 +5,42 @@ exports.signUp = async (req, res) => {
   const { username, password } = req.body;
   try {
     const hashpassword = await bcrypt.hash(password, 12);
+
     const user = await User.create({
       username,
       password: hashpassword,
     });
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: 'success',
+      data: { user },
+    });
+  } catch (err) {
+    res.status(400).json({ status: 'fail' });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      res.status(404).json({ status: 'fail', message: 'user not found' });
+    }
+
+    const isCorrectPassword = bcrypt.compare(password, user.password);
+
+    if (!isCorrectPassword) {
+      res
+        .status(400)
+        .json({ status: 'fail', message: 'incorrect credentials' });
+    }
+
+    user.password = undefined;
+
     res.status(201).json({
       status: 'success',
       data: { user },
